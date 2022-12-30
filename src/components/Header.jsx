c3f06a72a08e37bc382f4f66507ac451a2138c88
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-30 17:44:07 -0300

    Cambio de estilo en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,23 +9,33 @@ const Header = ({ changeMode }) => {
   const isDesktopScreen = useMediaQuery('(min-width: 1000px)');
 
   return (
-    <Box
-      display='flex'
-      justifyContent='space-between'
-      alignItems='center'
-      bgcolor={gray}
-      px={isDesktopScreen ? 10 : 2}
-      py={isDesktopScreen ? 3 : 4}
-      sx={{ boxShadow: 3 }}
-    >
-      <Typography fontWeight={800} variant={isDesktopScreen ? 'h2' : 'h4'}>
-        Where in the world?
-      </Typography>
-      <Box display='flex' alignItems='center' gap={1} onClick={changeMode} sx={{cursor: "pointer"}}>
-        {theme.palette.mode === 'dark' ? <LightMode /> : <ModeNight bgcolor={"red"}/>}
-        <Typography variant='h4'>
-          {theme.palette.mode === 'dark' ? 'Light' : 'Dark'} mode
+    <Box bgcolor={gray} py={isDesktopScreen ? 3 : 4} sx={{ boxShadow: 3 }}>
+      <Box
+        width='90%'
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+        m="0 auto"
+      >
+        <Typography fontWeight={800} variant={isDesktopScreen ? 'h2' : 'h4'}>
+          Where in the world?
         </Typography>
+        <Box
+          display='flex'
+          alignItems='center'
+          gap={1}
+          onClick={changeMode}
+          sx={{ cursor: 'pointer' }}
+        >
+          {theme.palette.mode === 'dark' ? (
+            <LightMode />
+          ) : (
+            <ModeNight bgcolor={'red'} />
+          )}
+          <Typography variant='h4'>
+            {theme.palette.mode === 'dark' ? 'Light' : 'Dark'} mode
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
